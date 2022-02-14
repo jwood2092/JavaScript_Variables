080ab0c5d3922c434e5de18673574462d7f996b4
justin woodard <jwoodard2092@gmail.com>
2022-02-14 14:09:10 -0600

    javascript varaibles

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName="Justin";
+let lastName='Woodard';
+var age='29';
+let isright="true";
 // Exercise 2
+let fullName = firstName + " " + lastName;
+console.log(fullName);
 
+// Bonus
+fullName= '${firstName} ${lastName}';
 // Exercise 3
+
+let myCity = "Hoover";
+let passTime = "Coding";
+let myStory = "Hi! my name is ${fullName}. I live in ${myCity}. I enjoy ${passTime}";
+console.log(myStory);
